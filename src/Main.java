373a0d6a70b8393c1756e0da032d23bd0bcc4190
Maria <melnikovamaria1708@gmail.com>
2022-12-23 17:14:05 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 6000;
+        int bonus = 20;
+
+        int bonusMiles = ticketPrice / bonus;
+        System.out.println(bonusMiles);
+    }
+}
